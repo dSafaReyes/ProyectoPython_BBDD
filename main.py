d020097a30b899ba286ed7be87e58f333836a49a
Jugador <dgavilanpedrozo@safareyes.es>
2021-11-18 20:34:57 +0100

    18/11/2021 - Commit 2 - Creados métodos crear_bbd() y eliminar_bbdd()

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
+import mysql.connector
+
+def crear_bbdd():
+    conexion = mysql.connector.connect(
+        host="localhost",
+        port="3306",
+        user="root",
+        password="raiz"
+    )
+    mycursor = conexion.cursor()
+    mycursor.execute("CREATE DATABASE proyectopython_bbdd")
+    mycursor.close()
+    return True
+
+def eliminar_bbdd():
+    conexion = mysql.connector.connect(
+        host="localhost",
+        port="3306",
+        user="root",
+        password="raiz"
+    )
+    mycursor = conexion.cursor()
+    mycursor.execute("DROP DATABASE proyectopython_bbdd")
+    mycursor.close()
+    return True
+
 if __name__ == '__main__':
-    ...
+    print(eliminar_bbdd())
