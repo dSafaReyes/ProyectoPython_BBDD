344e0fab40d329c1c2133c1c60da7e9055bcfbea
Jugador <dgavilanpedrozo@safareyes.es>
2021-11-26 20:34:23 +0100

    Commit 2: Pruebas

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 import mysql.connector
 
-def crear_bbdd():
-    conexion = mysql.connector.connect(
-        host="localhost",
-        port="3306",
-        user="root",
-        password="raiz"
-    )
+def conectar_bbdd():
+    conexion = mysql.connector.connect(host="localhost", port="3306", user="root", password="raiz")
+    cursor = conexion.cursor()
+    return cursor
+
+def crear_bbdd(nombre_bbdd):
+    conexion = conectar_bbdd()
     mycursor = conexion.cursor()
-    mycursor.execute("CREATE DATABASE proyectopython_bbdd")
+    mycursor.execute(f"CREATE DATABASE {nombre_bbdd}")
     mycursor.close()
     return True
 
-def eliminar_bbdd():
-    conexion = mysql.connector.connect(
-        host="localhost",
-        port="3306",
-        user="root",
-        password="raiz"
-    )
-    mycursor = conexion.cursor()
-    mycursor.execute("DROP DATABASE proyectopython_bbdd")
+def usar_bbdd(nombre_bbdd):
+    mycursor = conectar_bbdd()
+    mycursor.execute(f"USE {nombre_bbdd}")
+    return True
+
+def crear_tabla(nombre_tabla, nombre_bbdd):
+    mycursor = conectar_bbdd()
+    usar_bbdd(nombre_bbdd)
+    mycursor.execute(f"CREATE TABLE {nombre_tabla} (id int(3), nombre varchar(150), constraint jugador_pk primary key (id)")
     mycursor.close()
     return True
+
+def eliminar_bbdd(nombre_bbdd):
+    mycursor = conectar_bbdd()
+    mycursor.execute(f"DROP DATABASE {nombre_bbdd}")
+    mycursor.close()
+    return True
+
 
 if __name__ == '__main__':
-    print(eliminar_bbdd())
+    print(eliminar_bbdd("proyecto_python_bbdd"))
+    #print(crear_bbdd("proyecto_python_bbdd"))
+    #print(crear_tabla("jugador", "proyecto_python_bbdd"))
